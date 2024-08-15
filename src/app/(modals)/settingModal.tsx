2c80137024ecd6d4092611169f7fdf950713dcfb
gyc-12 <1450009389@qq.com>
2024-08-15 20:56:28 +0800

    refactor:更改应用名称 v1.0.4

--- src/app/(modals)/settingModal.tsx
+++ src/app/(modals)/settingModal.tsx
@@ -48,7 +48,7 @@ const settingsData = [
   {
     title: '应用信息',
     data: [
-      { id: '1', title: 'music-player', type: 'link', icon: require('@/assets/144.png') },
+      { id: '1', title: 'CyMusic', type: 'link', icon: require('@/assets/144.png') },
       { id: '2', title: '版本号', type: 'value', value: CURRENT_VERSION },
       { id: '3', title: '检查更新', type: 'value' },
       { id: '5', title: '项目链接', type: 'value', value: '' },
